b10b0783da71a0d52de821d0c3f01a8c48bb314b
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-12-23 01:02:43 +0200

    Update install package file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
-#!/usr/bin/pup
-# Install an specific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# 1-install_a_package.pp
+
+package { 'python3-pip':
+  ensure => present,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin', '/usr/local/bin'],
+  refreshonly => true,
+}
+
+package { 'Flask':
+  ensure  => '2.1.0',
+  require => Exec['install_flask'],
 }

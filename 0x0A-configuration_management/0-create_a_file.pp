cdfe42837f1053c6a438a38a7f90fe066e1d11c7
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-12-23 00:46:41 +0200

    install_a_package

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # creates a file in /tmp
 
-file { '/tmp/school':
-  content =>'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
+file {'/tmp/school':
+ensure  => present,
+content => 'I love Puppet',
+group   => 'www-data',
+owner   => 'www-data',
+mode    => '0744'
 }

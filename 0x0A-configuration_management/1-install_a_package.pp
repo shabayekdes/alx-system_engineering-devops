a1b7aa542017e3fd144af9582b0461854a57c1f1
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-01-03 00:48:00 +0200

    Fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,32 +1,5 @@
-# 1-install_a_package.pp
-
-class { 'python':
-  version => 'system',
-}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-package { 'build-essential':
-  ensure => installed,
-}
-
-package { 'libssl-dev':
-  ensure => installed,
-}
-
-package { 'libffi-dev':
-  ensure => installed,
-}
-
-package { 'python3-dev':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install --user flask==2.1.0',
-  path    => ['/bin', '/usr/bin'],
-  unless  => '/usr/bin/flask --version | grep "Flask 2.1.0"',
-  require => Package['python3-pip'],
+# Install flask v2.1.0 from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }

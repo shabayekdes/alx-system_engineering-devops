a9462412a81f76c359d7f6a3a9e7215063da66ff
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-02-15 00:12:46 +0200

    Add subs api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16.api.advanced:linux (by /shabayekdes)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    return response.json().get("data").get("subscribers")

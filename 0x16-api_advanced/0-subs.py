2e264f93c6d2feb6baf57e0d05f4a99745b2060b
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-02-15 00:26:39 +0200

    Update sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,9 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16.api.advanced:linux (by /shabayekdes)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/shabayekdes)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 404:
         return 0
-    return response.json().get("data").get("subscribers")
+    return res.get("data", {}).get("subscribers", 0)
